e177b3941b7f2bed139c1bc6cedd768f306fdf55
LikhithaYarlagadda <likhithayarlagadda57@gmail.com>
2020-10-12 21:58:24 +0530

    add structs in getPost

--- fbpost.go
+++ fbpost.go
@@ -22,13 +22,13 @@ func main() {
     e.POST("/login", loginUser)
     r := e.Group("/restricted")
     r.Use(middleware.JWT([]byte("secret")))
-    r.POST("/post", createPost)
-    r.POST("/comment", createComment)
-    r.POST("/reply", createReply)
-    r.GET("/post", getPosts)
-    r.GET("/user/posts", getUserPosts)
-    r.POST("/post/react", reactPost)
-    r.GET("/reactions", getReactions)
+    e.POST("/post", createPost)
+    e.POST("/comment", createComment)
+    e.POST("/reply", createReply)
+    e.GET("/post", getPosts)
+    e.GET("/user/posts", getUserPosts)
+    e.POST("/post/react", reactPost)
+    e.GET("/reactions", getReactions)
     e.Logger.Fatal(e.Start(":8080"))
 }
 type Claims struct {
@@ -77,14 +77,19 @@ func deleteUser(c echo.Context) error {
     db.Delete(&user, 2)
     return c.JSON(http.StatusOK, user)
 }
+
+type UpdateUser struct{
+    Username  string `json:"username"`
+}
 func updateUser(c echo.Context) error {
     db, err := gorm.Open(sqlite.Open("db.sqlite3"), &gorm.Config{})
     if err != nil {
         panic("error in connecting database")
     }
     var user models.User
+    var updated_user UpdateUser
     db.First(&user)
-    db.Model(&user).Updates(models.User{Username: "update"})
+    db.Model(&user).Updates(models.User{Username:updated_user.Username})
     return c.JSON(http.StatusOK, user)
 }
 func loginUser(c echo.Context) error {
@@ -128,12 +133,13 @@ func _get_token(username, password string) interface{} {
 type Post struct {
     Content string `json:"content"`
     PostedAt time.Time `json:"posted_at"`
+    UserId uint `json:"user_id"`
 }
 func createPost(c echo.Context) error {
     
-    user := c.Get("user").(*jwt.Token)
-    claims := user.Claims.(jwt.MapClaims)
-    user_id := uint(claims["id"].(float64))
+    // user := c.Get("user").(*jwt.Token)
+    // claims := user.Claims.(jwt.MapClaims)
+    // // user_id := uint(claims["id"].(float64))
     
     var post_details Post
     data_err := json.NewDecoder(c.Request().Body).Decode(&post_details)
@@ -146,15 +152,38 @@ func createPost(c echo.Context) error {
     if err != nil {
         panic("error in connecting database")
     }
-    post := &models.Post{Content: post_details.Content, PostedBy: user_id}
+    post := &models.Post{Content: post_details.Content, PostedBy: post_details.UserId}
     db.AutoMigrate(&models.Post{})
     db.Create(&post)
     var user_data models.User
-    db.First(&user_data, user_id)
+    db.First(&user_data, post_details.UserId)
     db.Model(&user_data).Association("Post").Append(post)
     db.Save(&user_data)
     return c.JSON(http.StatusOK, post)
 }
+type PostId struct{
+    PostId uint `json:post_id`
+}
+
+func deletePost(c echo.Context) error {
+    user := c.Get("user").(*jwt.Token)
+    claims := user.Claims.(jwt.MapClaims)
+    user_id := uint(claims["id"].(float64))
+    db, err := gorm.Open(sqlite.Open("db.sqlite3"), &gorm.Config{})
+    if err != nil {
+        panic("error in connecting database")
+    }
+    var to_be_deleted_post PostId
+    var user_id_of_post User
+    post_details := db.First(&to_be_deleted_post)
+    if post_details.PostedBy == user_id{
+        panic("Invalid Input")
+    }
+    db.Delete(&Post{}, to_be_deleted_post.PostId)
+    return c.String(http.StatusOK, "successfully created")
+
+}
+
 type Comment struct {
     Content string `json:"content"`
     PostId uint `json:"post_id"`
@@ -166,7 +195,7 @@ func createComment(c echo.Context) error {
     var comment_details Comment
     data_err := json.NewDecoder(c.Request().Body).Decode(&comment_details)
     if data_err != nil{
-        panic("invalid data")
+        panic("Invalid input")
     }
     db, err := gorm.Open(sqlite.Open("db.sqlite3"), &gorm.Config{})
     if err != nil {
@@ -206,14 +235,51 @@ func createReply(c echo.Context) error {
     db.Save(&comment)
     return c.JSON(http.StatusOK, reply)
 }
+
+type User struct {
+    UserId uint
+    Username string
+}
+
+type PostDetails struct {
+	PostId        int           `json:"post_id"`
+	PostedBy      User          `json:"posted_by"`
+	PostedAt      string        `json:"posted_at"`
+	PostContent   string        `json:"post_content"`
+	Reactions     Reactions     `json:"reactions"`
+	Comments      []CommentDict `json:"comments"`
+	CommentsCount int64         `json:"comments_count"`
+}
+
+type Reactions struct {
+	Count     int64    `json:"count"`
+	Reactions []string `json:"reactions"`
+}
+
+type CommentDict struct {
+	Id             int         `json:"comment_id"`
+	CommentedBy    User        `json:"commenter"`
+	CommentedAt    string      `json:"commented_at"`
+	CommentContent string      `json:"comment_content"`
+	ReactionsDict  Reactions   `json:"reactions"`
+	Replies        []ReplyDict `json:"replies"`
+	RepliesCount   int64       `json:"replies_count"`
+}
+
+type ReplyDict struct {
+	Id           int64  `json:"comment_id"`
+	RepliedBy    User   `json:"commenter"`
+	RepliedAt    string `json:"commented_at"`
+	ReplyContent string `json:"comment_content"`
+}
 func getPosts(c echo.Context) error {
     db, err := gorm.Open(sqlite.Open("db.sqlite3"), &gorm.Config{})
     if err != nil {
-        panic("error in  connecting database")
+        panic("failed to connect database")
     }
     var posts []models.Post
     // db.Table("posts").Joins("left join comments on comments.post_id = posts.id").Scan(&posts)
-    db.Joins("Comment").Find(&posts)
+    db.Joins("Comment").Find(&posts) 
     return c.JSON(http.StatusOK, posts)
 }
 func getUserPosts(c echo.Context) error {
@@ -240,7 +306,7 @@ func reactPost(c echo.Context) error {
     var reaction_details Reaction
     data_err := json.NewDecoder(c.Request().Body).Decode(&reaction_details)
     if data_err != nil{
-        panic("invalid data")
+        panic("invalid input")
     }
     db, err := gorm.Open(sqlite.Open("db.sqlite3"), &gorm.Config{})
     if err != nil {
@@ -267,6 +333,42 @@ func reactPost(c echo.Context) error {
     return c.String(http.StatusOK, "successfully created")
     
 }
+
+func reactComment(c echo.Context) error {
+    user := c.Get("user").(*jwt.Token)
+    claims := user.Claims.(jwt.MapClaims)
+    user_id := uint(claims["id"].(float64))
+    var reaction_details Reaction
+    data_err := json.NewDecoder(c.Request().Body).Decode(&reaction_details)
+    if data_err != nil{
+        panic("invalid input")
+    }
+    db, err := gorm.Open(sqlite.Open("db.sqlite3"), &gorm.Config{})
+    if err != nil {
+        panic("error in connecting database")
+    }
+    db.AutoMigrate(&models.Reaction{})
+    var old_reaction, new_reaction models.Reaction
+    error := db.First(&old_reaction, "reacted_by = ? and comment_id = ?", user_id, reaction_details.CommentId).Error
+    if error != nil {
+        new_reaction = models.Reaction{
+            CommentID: reaction_details.CommentId, ReactionType: reaction_details.ReactionType, 
+            ReactedBy: user_id,
+        }
+        db.Create(&new_reaction)
+        return c.String(http.StatusOK, "successfully created")
+    }
+    is_delete := old_reaction.ReactionType == reaction_details.ReactionType
+    if is_delete{
+        db.Delete(&old_reaction, old_reaction.ID)
+    } else {
+        old_reaction.ReactionType = reaction_details.ReactionType
+        db.Save(&old_reaction)
+    }
+    return c.String(http.StatusOK, "successfully created")
+    
+}
+
 type ReactionMetrics struct{
     ReactionType string `json:"reaction_type"`
     ReactionCount int `json:"reaction_count"`
